f425ccfb1291b947a08909a42cd37e9565ed0d63
gizemgllu <120049270+gizemgllu@users.noreply.github.com>
2023-03-07 17:20:17 +0300

    Lab1_1

---
+++ Lab1_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    string id;
+    cout << "What is your name?" <<endl;
+    cin>>name;
+    cout<<"Hello "<<name<<"."<<endl;
+    cout << "What is your student ID?" <<endl;
+    cin>>id;
+    cout<<"Your ID is "<<id<<"."<<endl;
+
+
+
+    return 0;
+}
